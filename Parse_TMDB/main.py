0893f44e1f52f84568ef662e1546d37cfdc223e6
KOJIEHO <nmi2002@bk.ru>
2025-03-19 01:15:23 +0300

    update

--- Parse_TMDB/main.py
+++ Parse_TMDB/main.py
@@ -1,13 +1,37 @@
 import requests
+from config import API_KEY
+# url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ru-RU&page=1"
 
-url = "https://tmdb.kurwa-bober.ninja/3/discover/movie?include_adult=false&include_video=false&language=ru-RU&page=1&sort_by=popularity.desc"
-# url = "https://api.themoviedb.org/3/movie/changes?page=1"
+# url для получения информации о фильме
+# url = "https://api.themoviedb.org/3/movie/696506?language=ru-RU"
 
+url = "https://api.themoviedb.org/3/movie/11?language=ru-RU"
+
+
+# url = 'https://image.tmdb.org/t/p/w500/dsHWBit2DkXohujpBIJL7ftPUQF.jpg'
+
+
+proxies = {
+   'https': 'http://67.43.236.21:31129'
+}
 
 headers = {
     "accept": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
 }
+try:
+    response = requests.get(url, headers=headers, proxies=proxies)
+    print(response.status_code)
+    print(response.text)
+except Exception as inst:
+    print('Видимо прокси сломался((')
 
-response = requests.get(url, headers=headers)
 
-print(response.text)
+# url = 'https://httpbin.io/ip'
+# response = requests.get(url, proxies=proxies)
+
+# img_file = open('photo_original.jpg', 'wb')
+# img_file.write(response.content)
+# img_file.close()
+
+
